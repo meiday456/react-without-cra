598f5ac7801d024d4d702b4eb05d74237334a6c8
meidya456 <sjkang0228@gmail.com>
2023-03-06 14:14:13 +0900

    type.d.ts 수정 module svg 내부에 필불요 코드 삭제

--- type.d.ts
+++ type.d.ts
@@ -1,44 +1,41 @@
+import type * as React from "react";
 
-declare module '*.svg' {
-    import * as React from 'react';
+export const ReactComponent: React.FunctionComponent<
+  React.SVGProps<SVGSVGElement> & { title?: string }
+>;
 
-    export const ReactComponent: React.FunctionComponent<React.SVGProps<
-        SVGSVGElement
-    > & { title?: string }>;
-
-    const src: string;
-    export default src;
+declare module "*.svg" {
+  const src: string;
+  export default src;
 }
 
-declare module '*.woff'
-declare module '*.woff2'
-declare module '*.eot'
-declare module '*.ttf'
-declare module '*.otf'
+declare module "*.woff";
+declare module "*.woff2";
+declare module "*.eot";
+declare module "*.ttf";
+declare module "*.otf";
 
-
-declare module '*.jpg' {
-    const src: string;
-    export default src;
+declare module "*.jpg" {
+  const src: string;
+  export default src;
 }
 
-declare module '*.jpeg' {
-    const src: string;
-    export default src;
+declare module "*.jpeg" {
+  const src: string;
+  export default src;
 }
 
-declare module '*.png' {
-    const src: string;
-    export default src;
+declare module "*.png" {
+  const src: string;
+  export default src;
 }
 
-
-declare module '*.bmp' {
-    const src: string;
-    export default src;
+declare module "*.bmp" {
+  const src: string;
+  export default src;
 }
 
-declare module '*.gif' {
-    const src: string;
-    export default src;
+declare module "*.gif" {
+  const src: string;
+  export default src;
 }
